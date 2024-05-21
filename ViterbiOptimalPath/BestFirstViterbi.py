4da54572b15ffcece659ff61196ee662d71100d2
TimeEfficientDP <115430764+TimeEfficientDP@users.noreply.github.com>
2024-05-21 21:49:57 +0300

    Update BestFirstViterbi.py

--- ViterbiOptimalPath/BestFirstViterbi.py
+++ ViterbiOptimalPath/BestFirstViterbi.py
@@ -331,3 +331,222 @@ def bidirectional_mint_bound(G, start, y, last_nodes):
            break 
                 
    return  best_path , mu
+
+
+
+def mint_dfs(G, all_nodes, start, frames, space_budget_global, y,
+                                   final_state = None, start_frame = 0, last_frame = None,
+                                   n_frames = None,  middle_pairs = []):
+    
+    
+    '''
+    mint-dfs 
+    Parameters: 
+        G: transition graph (graph object) 
+        all_nodes: graph nodes (list) 
+        start: start state (int)
+        space_budget_global: space budget (float)
+        y: vector of observations (list) 
+        final_state: if available, last state (int) 
+        start_frame: start frame (int) 
+        last_frame: last frame (int) 
+        middle_pairs: current middle pairs (list) 
+        
+            
+    Returns 
+    middle_pairs: final middle pairs (list)
+    '''
+   
+    # init 
+    T = len(frames) 
+    th = ceil((frames[0] + frames[-1])/2)   
+    D = defaultdict(float) 
+    D[(start, start_frame) ] = ( -G.emission_probabilities[ start ][ y[ start_frame ] ] , -1, (-1,-1) )         # mapping of nodes to their dist from start
+    Q = PQDict(D)          
+    stored_tokens = set() 
+    stored_tokens.add( (start, start_frame) ) 
+    visited = set() 
+        
+    if last_frame == None:
+        last_frame = T-1 
+                
+    if n_frames == None:
+        n_frames = T    
+   
+
+    while Q:             
+                       
+        (k, val) = Q.popitem()   
+        d, pred , med_pair = val #unpack distance and optimal predecessor 
+        stored_tokens.remove(k)        
+        
+        D[k]=d                           # est dijkstra greedy score
+        visited.add(k)                   # remove from unexplored
+
+        if k[1]==th: 
+            if med_pair == (-1,-1): 
+                med_pair = (pred, k[0]) 
+                
+        if (k[1] == last_frame and final_state is None) or (k[0] == final_state and k[1] == last_frame): 
+         
+            x_a, x_b =  med_pair 
+                      
+            N_left = floor(len(frames)/2)      
+            
+            if N_left >1 : 
+                
+                left_frames = frames[:N_left]
+                start_frame = left_frames[0]
+                last_frame = left_frames[-1]
+                ancestors = BFS_ancestors(G, x_a,  N_left)
+                mint_dfs(G, ancestors.union({x_a}), start, left_frames, space_budget_global, y, final_state = x_a, start_frame = start_frame, last_frame = last_frame, n_frames = N_left, middle_pairs = middle_pairs)
+                        
+            middle_pairs.append((x_a, x_b)) 
+            N_right = len(frames) - N_left
+            
+            if N_right >1: 
+                
+                right_frames = frames[-N_right:]
+                start_frame_right = right_frames[0]
+                last_frame_right = right_frames[-1]
+                descendants = BFS_descendants(G, x_b,  N_right)
+                mint_dfs(G, descendants.union({x_b}), x_b, right_frames, space_budget_global, y, final_state = final_state, start_frame =   start_frame_right , last_frame = last_frame_right , n_frames = N_right, middle_pairs = middle_pairs)
+            break
+            
+        
+        for w in G.adj[k[0]]:                        
+            if k[1]+1 > last_frame: 
+                break 
+            
+            if ( w , k[1]+1 ) not in visited and w in all_nodes:        
+                
+                d = D[k] - G.adj[k[0]][w]  - G.emission_probabilities[ w ][ y[k[1]+1] ]      # dgs: dist of start -> v -> w
+                tmp = Q.get( (w,k[1]+1), (float("inf") , -1, (-1,1))   )[0]
+                    
+                if d < tmp:
+                    if  len(stored_tokens) > space_budget_global and (w, k[1]+1) not in stored_tokens: 
+                        # running dfs 
+                        Q, stored_tokens = dfs(G, all_nodes, k[0], w, k[1]+1, d, med_pair, Q, stored_tokens, y, set(), space_budget_global, n_frames, len(stored_tokens), th)                          
+                        
+                    else:
+                        # update queue ´
+                        Q[(w, k[1]+1)] = (d, k[0], med_pair)   
+                        stored_tokens.add( (w, k[1]+1) )
+    
+    return middle_pairs 
+
+
+def dfs( G, all_nodes, pred, state, frame, cost, med_pair, Q, stored_tokens,  y, visited_dfs, space_budget, n_frames, tot_tokens, th): 
+                    
+    ''' function invoked by mint_dfs to perform dfs '''
+        
+    if frame==th: 
+        med_pair = (pred, state) 
+    
+    if frame < n_frames-1: 
+        for neighbour in G.adj[state]:
+            if neighbour in all_nodes:
+                this_cost = cost - G.adj[state][neighbour]  - G.emission_probabilities[ neighbour ][ y[frame+1] ]                
+    
+                if (neighbour,frame+1) in stored_tokens: 
+                        tmp = Q.get((neighbour,frame+1), float("inf"))
+                        if isinstance(tmp, tuple): 
+                            tmp = tmp[0] 
+                            
+                        if this_cost < tmp:
+                            Q[(neighbour, frame+1)] = (this_cost , state, med_pair)
+
+                else:
+                    Q, stored_tokens  = dfs(G=G, all_nodes=all_nodes, pred=state, state=neighbour, frame= frame+1, cost= this_cost ,
+                                                                      med_pair = med_pair , Q = Q , stored_tokens = stored_tokens , y = y, visited_dfs=visited_dfs, 
+                                                                      space_budget=space_budget, n_frames=n_frames, tot_tokens=tot_tokens, th=th)
+    else: 
+        tmp =  Q.get((state,frame), (float("inf"), -1, (-1,-1)))[0]
+        if cost < tmp:                 
+            Q[(state, frame)] = (cost, pred, med_pair)
+            stored_tokens.add(  (state,frame)    ) 
+            
+    return Q, stored_tokens
+
+
+
+def BFS_ancestors(G, source, b):
+ 
+        ''''
+        function invoked by mint_dfs to perform single-source BFS traversal of ancestors up to b hops 
+        
+        '''
+        
+        # Mark all the vertices as not visited
+        visited = set() 
+
+        # Create a queue for BFS
+        queue = []
+ 
+        # Mark the source node as 
+        # visited and enqueue it
+        queue.append(source)
+        queue.append("null") # for level 
+      
+        level = 0 
+        
+        while queue and level < b:
+            # Dequeue a vertex from 
+            # queue and print it
+            s = queue.pop(0)
+                        
+            if s == "null": 
+                level += 1 # you increase one level everytime you encounter a null 
+                queue.append("null") 
+                
+            else: 
+                # Get all adjacent vertices of the
+                # dequeued vertex s. If a adjacent
+                # has not been visited, then mark it
+                # visited and enqueue it
+                for state_idx in  G.adj_inv[s]:                     
+                    if state_idx not in visited: 
+                        queue.append(state_idx)
+                        visited.add(state_idx)
+                
+        return visited
+
+
+
+def BFS_descendants(G, source,  b):
+ 
+        ''''
+        function invoked by mint_dfs to perform traversal of descendants up to b hops 
+        
+        '''
+        
+        # Mark all the vertices as not visited
+        visited = set() 
+        # Create a queue for BFS
+        queue = []
+        # Mark the source node as 
+        # visited and enqueue it
+        queue.append(source)
+        queue.append("null") # for level 
+        #visited.add(source)
+        level = 0 
+      
+        while queue and level < b:
+            # Dequeue a vertex from 
+            # queue and print it
+            s = queue.pop(0)
+            if s == "null": 
+                level += 1 # you increase one level everytime you encounter a null 
+                queue.append("null") 
+                
+            else: 
+                # Get all adjacent vertices of the
+                # dequeued vertex s. If a adjacent
+                # has not been visited, then mark it
+                # visited and enqueue it
+                for state_idx in G.adj[s]:
+                    if state_idx not in visited:
+                        queue.append(state_idx)
+                        visited.add(state_idx)
+                      
+        return visited 
